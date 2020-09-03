6478ea17bbc07649aab20c30ef59b820ae75957b
Sophia <fehrmannsf@vcu.edu>
2020-09-03 13:00:12 -0400

    Added NYT data endpoint

--- cavoider_api_backend/APIRequests.py
+++ cavoider_api_backend/APIRequests.py
@@ -1,20 +1,36 @@
 from pathlib import Path
 import requests
+import csv
 from cavoider_api_backend import conf
+import pandas
+import tempfile
+import io
 
 
 def get_data_from_endpoint(endpoint):
-    return requests.get(endpoint).json()
-
+    file_extension = endpoint.split(".")[-1]
+    if (file_extension == "json"):
+        response = requests.get(endpoint).json()
+        df = pandas.DataFrame.from_dict(response)
+        return df
+    else:
+        response = requests.get(endpoint)
+        df = pandas.read_csv(io.StringIO(response.content.decode('utf-8')))
+        return df
 
 def get_excess_deaths_from_cdc():
     endpoint = conf.CDC_EXCESS_DEATHS_ENDPOINT
     return get_data_from_endpoint(endpoint)
 
+def get_nyt_historical_data():
+    endpoint = conf.NYT_HISTORICAL_COUNTIES_ENDPOINT
+    return get_data_from_endpoint(endpoint)
+
+def get_nyt_current_data():
+    endpoint = conf.NYT_CURRENT_COUNTIES_ENDPOINT
+    return get_data_from_endpoint(endpoint)
 
 if __name__ == "__main__":
-    import pandas
-    raw_response = get_excess_deaths_from_cdc()
-    df = pandas.DataFrame.from_dict(raw_response)
-    with Path("../out/excess_deaths.xlsx").open("wb") as f:
-        df.to_excel(f)
+    df = get_nyt_historical_data()
+    #with Path("../out/nytHistorical.xlsx").open("wb") as f:
+    #    df.to_excel(f)
--- cavoider_api_backend/conf.py
+++ cavoider_api_backend/conf.py
@@ -5,6 +5,10 @@ env = Env()
 env.read_env()
 
 CDC_EXCESS_DEATHS_ENDPOINT = env.str("CDC_EXCESS_DEATHS_ENDPOINT") #=> https://data.cdc.gov/resource/r8kw-7aab.json
+NYT_HISTORICAL_COUNTIES_ENDPOINT = env.str("NYT_HISTORICAL_COUNTIES_ENDPOINT") #=> https://github.com/nytimes/covid-19-data/raw/master/us-counties.csv
+NYT_CURRENT_COUNTIES_ENDPOINT = env.str("NYT_CURRENT_COUNTIES_ENDPOINT") #=>https://github.com/nytimes/covid-19-data/raw/master/live/us-counties.csv
 
 if __name__ == "__main__":
     print(CDC_EXCESS_DEATHS_ENDPOINT)
+    print(NYT_CURRENT_COUNTIES_ENDPOINT)
+    print(NYT_HISTORICAL_COUNTIES_ENDPOINT)
