031f4890af89623033243df6fc345e98b96f3685
LiamMeron <LiamMeron@users.noreply.github.com>
2020-09-05 17:20:50 -0400

    Added Caching to only download data sources once a day

--- cavoider_api_backend/APIRequests.py
+++ cavoider_api_backend/APIRequests.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 import requests
 import csv
@@ -5,18 +6,30 @@ from cavoider_api_backend import conf
 import pandas
 import tempfile
 import io
+from urllib.parse import urlparse
+from datetime import datetime
+from pathlib import Path
 
+CACHE_PATH = Path("../res/cache/")
 
 def get_data_from_endpoint(endpoint):
-    file_extension = endpoint.split(".")[-1]
-    if (file_extension == "json"):
-        response = requests.get(endpoint).json()
-        df = pandas.DataFrame.from_dict(response)
+    today = datetime.now().date().isoformat()
+    filename = f"{today}_{os.path.basename(urlparse(endpoint).path)}"
+    file_extension = filename.split(".")[-1]
+    file_cache_path = Path(CACHE_PATH) / filename
+    if file_cache_path.exists():
+        if file_extension == "json":
+            df = pandas.read_json(file_cache_path)
+        elif file_extension == "csv":
+            df = pandas.read_csv(file_cache_path)
+        else:
+            raise ValueError(f"File Extension: {file_extension}")
         return df
     else:
-        response = requests.get(endpoint)
-        df = pandas.read_csv(io.StringIO(response.content.decode('utf-8')))
-        return df
+        with open(file_cache_path, mode="wb") as f:
+            response = requests.get(endpoint)
+            f.write(response.content)
+        return get_data_from_endpoint(endpoint)
 
 def get_excess_deaths_from_cdc():
     endpoint = conf.CDC_EXCESS_DEATHS_ENDPOINT
@@ -35,8 +48,7 @@ def get_current_county_data():
     return get_data_from_endpoint(endpoint)
 
 if __name__ == "__main__":
-    df = get_current_county_data()
-    print(df.head())
-    print(df.tail())
+    get_nyt_current_data() #CSV
+    get_excess_deaths_from_cdc() #JSON
     #with Path("../out/nytHistorical.xlsx").open("wb") as f:
     #    df.to_excel(f)
