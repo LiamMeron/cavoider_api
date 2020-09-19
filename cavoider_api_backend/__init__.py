cd08c40dcb7a08367ffa4b80ea68bcb1648c9ea9
Sophia <fehrmannsf@vcu.edu>
2020-09-19 13:13:00 -0400

    Updated init.py and calcualtions

--- cavoider_api_backend/__init__.py
+++ cavoider_api_backend/__init__.py
@@ -1,13 +1,3 @@
 
 
-# TODO Download data from CDC API
-
-# TODO Download data from NYT Dataset
-
-# TODO Calculate Positive Tests / Total Tests
-
-# TODO Calculate Population Density
-
-# TODO Calculate 14-day trend -> Place into buckets (increasing/flat/decreasing)
-
-# TODO Calculate Confidence Score Factor
+# TODO Decide on format from calculations for API
