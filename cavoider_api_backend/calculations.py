217fc888936b3f1c66164327bee401f1fb0a5d58
William Coleman <will.colmn@gmail.com>
2020-09-28 11:54:48 -0400

    black formatting

--- cavoider_api_backend/calculations.py
+++ cavoider_api_backend/calculations.py
@@ -5,28 +5,38 @@ from pandas import DataFrame
 import pandas
 
 
-
-#from cavoider_api_backend.repository import Partition, AzureTableRepository
+# from cavoider_api_backend.repository import Partition, AzureTableRepository
 
 
 # Calculate covid data per 100000 people
-def create_covid_data_by_population(covid_data: DataFrame, population: DataFrame, column_name: str):
-    df_covid_data_and_pop = covid_data.merge(population, left_on="fips", right_on="countyFIPS")
+def create_covid_data_by_population(
+    covid_data: DataFrame, population: DataFrame, column_name: str
+):
+    df_covid_data_and_pop = covid_data.merge(
+        population, left_on="fips", right_on="countyFIPS"
+    )
     df_covid_data_and_pop[f"{column_name}_per_100k_people"] = (
-        df_covid_data_and_pop[f"{column_name}"] / df_covid_data_and_pop["population"]) * 100000
+        df_covid_data_and_pop[f"{column_name}"] / df_covid_data_and_pop["population"]
+    ) * 100000
     return df_covid_data_and_pop[["fips", f"{column_name}_per_100k_people"]]
 
 
 # Calculate deaths per 100000 people
 def create_deaths_per_100k_people(current_data: DataFrame, population: DataFrame):
-    return create_covid_data_by_population(current_data[["fips", "deaths"]],
-                                           population[["countyFIPS", "population"]], "deaths")
+    return create_covid_data_by_population(
+        current_data[["fips", "deaths"]],
+        population[["countyFIPS", "population"]],
+        "deaths",
+    )
 
 
 # Calculate cases per 100000 people
 def create_cases_per_100k_people(current_data: DataFrame, population: DataFrame):
-    return create_covid_data_by_population(current_data[["fips", "cases"]],
-                                           population[["countyFIPS", "population"]], "cases")
+    return create_covid_data_by_population(
+        current_data[["fips", "cases"]],
+        population[["countyFIPS", "population"]],
+        "cases",
+    )
 
 
 # Calculate case fatality per 100000 people
@@ -35,7 +45,9 @@ def create_case_fatality_rate(deaths: DataFrame, cases: DataFrame):
 
 
 # Calculate difference between two different days
-def create_daily_dif_between_columns(column_name: str, historical_data: DataFrame, current_data: DataFrame):
+def create_daily_dif_between_columns(
+    column_name: str, historical_data: DataFrame, current_data: DataFrame
+):
     today = current_data.iloc[0, 0]
     today = datetime.fromisoformat(today)
     yesterday = str(today - timedelta(days=1)).split(" ")
@@ -48,12 +60,15 @@ def create_daily_dif_between_columns(column_name: str, historical_data: DataFram
     df_yesterday = df_yesterday[["fips", f"{column_name}"]]
     df_two_days_ago = historical_data[historical_data["date"] == two_days_ago]
     df_two_days_ago = df_two_days_ago[["fips", f"{column_name}"]]
-    df_two_days_ago = df_two_days_ago.rename(columns={f"{column_name}": f"prev_{column_name}"})
+    df_two_days_ago = df_two_days_ago.rename(
+        columns={f"{column_name}": f"prev_{column_name}"}
+    )
 
     df_daily_difference = df_yesterday.merge(df_two_days_ago, on="fips")
 
     df_daily_difference[f"new_daily_{column_name}"] = (
-        df_daily_difference[f"{column_name}"] - df_daily_difference[f"prev_{column_name}"]
+        df_daily_difference[f"{column_name}"]
+        - df_daily_difference[f"prev_{column_name}"]
     )
     return df_daily_difference[["fips", f"new_daily_{column_name}"]]
 
@@ -71,8 +86,12 @@ def create_daily_death_count(historical_data: DataFrame, current_data: DataFrame
 
 
 # Calculate difference between week
-def create_diff_between_columns(column_name: str, historical_data: DataFrame,
-                                       current_data: DataFrame, num_days_elapsed: int):
+def create_diff_between_columns(
+    column_name: str,
+    historical_data: DataFrame,
+    current_data: DataFrame,
+    num_days_elapsed: int,
+):
     today = current_data.iloc[0, 0]
     today = datetime.fromisoformat(today)
     elapsed_days = str(today - timedelta(days=num_days_elapsed)).split(" ")
@@ -87,7 +106,7 @@ def create_diff_between_columns(column_name: str, historical_data: DataFrame,
     df_difference = df_today.merge(df_elapsed, on="fips")
 
     df_difference[f"new_difference_{column_name}"] = (
-            df_difference[f"{column_name}"] - df_difference[f"prev_{column_name}"]
+        df_difference[f"{column_name}"] - df_difference[f"prev_{column_name}"]
     )
     return df_difference[["fips", f"new_difference_{column_name}"]]
 
@@ -105,7 +124,10 @@ def create_14_day_case_trend(historical_data: DataFrame, current_data: DataFrame
     # calculate the percent increase
     df_both_weeks = df_14_days.merge(df_week_2, on="fips")
     df_both_weeks["week 1"] = df_both_weeks["14 days"] - df_both_weeks["week 2"]
-    df_both_weeks["percent_change_14_days"] = ((df_both_weeks["week 2"] - df_both_weeks["week 1"]) / df_both_weeks["week 1"].abs()) * 100
+    df_both_weeks["percent_change_14_days"] = (
+        (df_both_weeks["week 2"] - df_both_weeks["week 1"])
+        / df_both_weeks["week 1"].abs()
+    ) * 100
 
     return df_both_weeks[["fips", "percent_change_14_days"]]
 
@@ -114,13 +136,19 @@ def create_14_day_case_trend(historical_data: DataFrame, current_data: DataFrame
 # (see COVID Tracking Project - The Atlantic for more info)
 def create_active_cases_estimate(historical_data: DataFrame, current_data: DataFrame):
     # calculate change in cases and deaths in last 30 days
-    df_30_day_cases = create_diff_between_columns("cases", historical_data, current_data, 30)
-    df_30_day_deaths = create_diff_between_columns("deaths", historical_data, current_data, 30)
+    df_30_day_cases = create_diff_between_columns(
+        "cases", historical_data, current_data, 30
+    )
+    df_30_day_deaths = create_diff_between_columns(
+        "deaths", historical_data, current_data, 30
+    )
     df_30_day_change = df_30_day_cases.merge(df_30_day_deaths, on="fips")
 
     # calculate active cases
-    df_30_day_change["active_cases_est"] = \
-        df_30_day_change["new_difference_cases"] - df_30_day_change["new_difference_deaths"]
+    df_30_day_change["active_cases_est"] = (
+        df_30_day_change["new_difference_cases"]
+        - df_30_day_change["new_difference_deaths"]
+    )
 
     return df_30_day_change[["fips", "active_cases_est"]]
 
@@ -133,14 +161,18 @@ def main():
 
     # Calculate all the statistics
     cases_per_100k_people = create_cases_per_100k_people(df_NYT_current, df_county_pop)
-    deaths_per_100k_people = create_deaths_per_100k_people(df_NYT_current, df_county_pop)
+    deaths_per_100k_people = create_deaths_per_100k_people(
+        df_NYT_current, df_county_pop
+    )
     daily_case_change = create_daily_case_count(df_NYT_historical, df_NYT_current)
     daily_death_change = create_daily_death_count(df_NYT_historical, df_NYT_current)
     case_trend_14_days = create_14_day_case_trend(df_NYT_historical, df_NYT_current)
     active_cases_est = create_active_cases_estimate(df_NYT_historical, df_NYT_current)
 
     # Create a master data table with all relevant statistics
-    df_current_data = df_NYT_current[["date", "county", "state", "fips", "cases", "deaths"]]
+    df_current_data = df_NYT_current[
+        ["date", "county", "state", "fips", "cases", "deaths"]
+    ]
     df_master = df_current_data.merge(cases_per_100k_people, on="fips")
     df_master = df_master.merge(deaths_per_100k_people, on="fips")
     df_master = df_master.merge(daily_case_change, on="fips")
@@ -156,12 +188,12 @@ def main():
     pandas.set_option("max_columns", None)
     print(df_master)
 
-    #df_to_dict = json.loads(df_master.head(n=200).to_json(orient="table", index=False))[
+    # df_to_dict = json.loads(df_master.head(n=200).to_json(orient="table", index=False))[
     #    "data"
-    #]
+    # ]
 
-    #repo = AzureTableRepository("Test01")
-    #for record in df_to_dict:
+    # repo = AzureTableRepository("Test01")
+    # for record in df_to_dict:
     #    repo.add(Partition.latest_county_report, record)
 
 
