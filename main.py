ae63d0cfcd268cf167dad43bb084833294bcc528
William Coleman <will.colmn@gmail.com>
2020-10-30 17:36:44 -0400

    Removed unwanted endpoint

--- main.py
+++ main.py
@@ -16,15 +16,6 @@ async def read_latest_report_for(fips: str):
     return data
 
 
-@app.get("/latest/")
-async def read_latest_report_for(q: str = Query(None)):
-    response = []
-    for item in q:
-        data = repo.get(partition=Partition.latest_county_report, row_key=f"{item}")
-        response.append(data)
-    return response
-
-
 @app.get("/county/{fips}")
 async def get_county_outline_for(fips: str):
     data = repo.get(partition=Partition.counties, row_key=fips)
